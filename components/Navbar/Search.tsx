1c8b706684d5dafe14419ed9576d8927250cf0cb
gagmister <gagan.majathia@gmail.com>
2023-04-23 01:34:14 -0400

    Added search to nav component. Still wip

--- components/Navbar/Search.tsx
+++ components/Navbar/Search.tsx
@@ -32,7 +32,7 @@ export default function Search(){
                     "  
 
                 >
-                    Somewhere
+
                 </div>
 
             </div>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,29 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useState } from 'react'
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { SessionContextProvider, Session } from '@supabase/auth-helpers-react'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+
+function MyApp({
+  Component,
+  pageProps,
+}: AppProps<{
+  initialSession: Session,
+}>) {
+  const [supabase] = useState(() => createBrowserSupabaseClient())
+
+  return (
+    <SessionContextProvider supabaseClient={supabase} initialSession={pageProps.initialSession}>
+      <Component {...pageProps} />
+    </SessionContextProvider>
+  )
 }
+export default MyApp
+
+
+
+// export default function App({ Component, pageProps }: AppProps) {
+//   return <Component {...pageProps} />
+// }
